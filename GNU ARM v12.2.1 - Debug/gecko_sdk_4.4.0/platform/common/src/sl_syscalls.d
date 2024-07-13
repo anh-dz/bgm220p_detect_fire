ff6fc26f90a1a69e99b96a74e05dc38cb7f6825b
Nhat Anh <nmnanh1235@gmail.com>
2024-07-13 09:44:34 +0700

    delete all

--- GNU ARM v12.2.1 - Debug/gecko_sdk_4.4.0/platform/common/src/sl_syscalls.d
+++
@@ -1,8 +0,0 @@
-gecko_sdk_4.4.0/platform/common/src/sl_syscalls.o: \
- C:/Users/Lenovo/Desktop/gecko-sdk/platform/common/src/sl_syscalls.c \
- c:\users\lenovo\desktop\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- c:\users\lenovo\desktop\gecko-sdk\platform\cmsis\core\include\cmsis_compiler.h \
- c:\users\lenovo\desktop\gecko-sdk\platform\cmsis\core\include\cmsis_gcc.h
-c:\users\lenovo\desktop\gecko-sdk\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-c:\users\lenovo\desktop\gecko-sdk\platform\cmsis\core\include\cmsis_compiler.h:
-c:\users\lenovo\desktop\gecko-sdk\platform\cmsis\core\include\cmsis_gcc.h:
--- readme.md
+++
@@ -1,2 +0,0 @@
-# Empty C Example
-This example project shows an empty configuration that can be used as a starting point to add components and functionality.
